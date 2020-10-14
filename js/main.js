174dceff490f5012449875b2cdcccc09d860cedf
Danila197 <levvyi197@gmail.com>
2020-10-14 14:14:07 +0500

    slider + slider adaptive

--- js/main.js
+++ js/main.js
@@ -6,4 +6,16 @@ $(function () {
       $('.menu-list').slideUp();
     }
   })
+  $('.service-slider').slick({
+    arrows: false,
+    dots: true,
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        dots: false,
+        centerMode: true,
+        centerPadding: '50px',
+      }
+    }]
+  })
 })
